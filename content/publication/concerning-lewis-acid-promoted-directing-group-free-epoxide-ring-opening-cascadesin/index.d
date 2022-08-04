e5819741b1ee145395f5c40ae62c2f336414d086
acrokeMIT <91698344+acrokeMIT@users.noreply.github.com>
2022-08-04 16:54:27 -0400

    Update index.d

--- content/publication/concerning-lewis-acid-promoted-directing-group-free-epoxide-ring-opening-cascadesin/index.d
+++ content/publication/concerning-lewis-acid-promoted-directing-group-free-epoxide-ring-opening-cascadesin/index.d
@@ -1,6 +1,5 @@
 ---
-title: Concerning Lewis Acid-Promoted, Directing Group-Free Epoxide Ring-Opening
-  Cascades
+title: Concerning Lewis Acid-Promoted, Directing Group-Free Epoxide Ring-Opening Cascades
 publication_types:
   - "2"
 authors:
@@ -8,12 +7,7 @@ authors:
   - Timothy F. Jamison
 publication: Synlett
 url_pdf: https://www.thieme-connect.com/products/ejournals/abstract/10.1055/s-2006-949637
-abstract: Lewis acid promoted cascades of a tris(disubstituted epoxide)
-  triggered by silver-promoted abstraction of a bromide ion favors
-  trans-dioxabicyclo[4.3.0]nonanes, rather than diads or triads of
-  tetrahydropyrans (trans-dioxabicylco[4.4.0]decanes, for example). These
-  results suggest that an epoxide-attacking-epoxonium mechanism is not operative
-  in this system.
+abstract: Lewis acid promoted cascades of a tris(disubstituted epoxide) triggered by silver-promoted abstraction of a bromide ion favors trans-dioxabicyclo[4.3.0]nonanes, rather than diads or triads of tetrahydropyrans (trans-dioxabicylco[4.4.0]decanes, for example). These results suggest that an epoxide-attacking-epoxonium mechanism is not operativein this system.
 draft: false
 featured: false
 tags:
@@ -22,5 +16,5 @@ image:
   filename: featured
   focal_point: Smart
   preview_only: false
-date: 2006-08-24T20:08:16.944Z
+date: 2006-08-24T20:08:16.944
 ---
